7c0ad29f6d1da841bb53a836e75ebdc650cbacb4
Mohd Ali <136788263+ali008DS@users.noreply.github.com>
2023-07-31 04:22:23 +0530

    Add files via upload

---
+++ ASSIGNME 22.py
@@ -0,0 +1,18 @@
+def find_closest_elements(arr, k, x):
+    left, right = 0, len(arr) - k
+
+    while left < right:
+        mid = left + (right - left) // 2
+        if x - arr[mid] > arr[mid + k] - x:
+            left = mid + 1
+        else:
+            right = mid
+
+    return arr[left:left + k]
+
+# Test cases
+print(find_closest_elements([1, 2, 3, 4, 5], 4, 3))
+# Output: [1, 2, 3, 4]
+
+print(find_closest_elements([1, 2, 3, 4, 5], 4, -1))
+# Output: [1, 2, 3, 4]
